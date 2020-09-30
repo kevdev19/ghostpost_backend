984d0b5268aff12f7b72d4613cac4a3611257c44
Kevin Blount <kvbchamp@gmail.com>
2020-09-29 20:30:35 -0400

    url and viewset config

--- api/views.py
+++ api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import RoastBoast
+from .serializers import RoastBoastSerializer
 
-# Create your views here.
+class RoastBoastViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows roast and boast content to be viewed and added
+    """
+
+    queryset = RoastBoast.objects.all()
+    serializer_class = RoastBoastSerializer
