f269bc08ce9a46e7e6822a8f8087501f5ad70ec9
Kevin Blount <kvbchamp@gmail.com>
2020-10-01 00:42:05 -0400

    modify choice field names for post type

--- api/models.py
+++ api/models.py
@@ -2,8 +2,8 @@ from django.db import models
 from django.utils.timezone import now
 
 class RoastBoast(models.Model):
-    POST_TYPE_CHOICES = [('B', 'Boast'), ('R', 'Roast'),]
-    post_type = models.CharField(max_length=1, blank=True, choices=POST_TYPE_CHOICES)
+    POST_TYPE_CHOICES = [('Boast', 'Boast'), ('Roast', 'Roast'),]
+    post_type = models.CharField(max_length=6, blank=True, choices=POST_TYPE_CHOICES)
     content = models.CharField(max_length=280, blank=True)
     up_vote = models.IntegerField(default=0)
     down_vote = models.IntegerField(default=0)
